ae6ec320fa41d32890ed4c002399e6881bcd73b0
John Brooke <bro16077@byui.edu>
2024-02-09 00:35:03 -0700

    Finish implementing cross componenet communication for contacts

--- contact-management-system/src/app/contacts/contacts.component.ts
+++ contact-management-system/src/app/contacts/contacts.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from './contact.model';
+import { ContactService } from './contact/contact.service';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css',
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
   selectedContact: Contact;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.contactService.contactSelectedEvent.subscribe((contact: Contact) => {
+      this.selectedContact = contact;
+    });
+  }
 }
