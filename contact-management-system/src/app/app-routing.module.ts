7b51a69baa700393aaf2b75b6f85c5add2193dba
John Brooke <bro16077@byui.edu>
2024-02-16 22:23:50 -0700

    add app-routing.module.ts file with appRoutes and NgModule()

---
+++ contact-management-system/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { Routes, RouterModule } from "@angular/router";
+import { DocumentsComponent } from "./documents/documents.component";
+import { MessageListComponent } from "./messages/message-list/message-list.component";
+import { ContactsComponent } from "./contacts/contacts.component";
+import { NgModule } from "@angular/core";
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/documents' },
+    { path: 'documents', component: DocumentsComponent },
+    { path: 'messages', component: MessageListComponent },
+    { path: 'contacts', component: ContactsComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
